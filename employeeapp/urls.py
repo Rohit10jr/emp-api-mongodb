fd2e490617dda9677e16b8375d0ac547056a61c3
Rohit <130643902+Rohit10jr@users.noreply.github.com>
2023-07-24 20:38:51 +0530

    Add files via upload

---
+++ employeeapp/urls.py
@@ -0,0 +1,18 @@
+# from django.conf.urls import path
+from django.urls import path
+from employeeapp import views 
+from django.urls import re_path as url
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns=[
+  url(r'^department$',views.departmentApi),
+  url(r'^department/([0-9]+)$',views.departmentApi),
+
+  url(r'^employee$',views.employeeApi),
+  url(r'^employee/([0-9]+)$',views.employeeApi), 
+
+  
+  url(r'^employee/savefile',views.SaveFile),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
